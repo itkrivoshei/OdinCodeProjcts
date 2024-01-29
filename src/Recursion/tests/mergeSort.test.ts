e4dfb9c0d6b15ca417c55f64a2823879fd127503
itkrivoshei <nikitakrivoshei@gmail.com>
2024-01-29 14:18:50 +0100

    Refactor mergeSort test case

--- src/Recursion/tests/mergeSort.test.ts
+++ src/Recursion/tests/mergeSort.test.ts
@@ -33,7 +33,9 @@ describe("mergeSort", () => {
     const largeArray = Array.from({ length: 1000 }, () =>
       Math.floor(Math.random() * 1000)
     );
+
     const sortedArray = [...largeArray].sort((a, b) => a - b);
+
     expect(mergeSort(largeArray)).toEqual(sortedArray);
   });
 });
