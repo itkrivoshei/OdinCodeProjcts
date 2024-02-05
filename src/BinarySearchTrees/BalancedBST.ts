2dcaa8aeae2158036bab7cd1107de0c78051a119
itkrivoshei <nikitakrivoshei@gmail.com>
2024-02-05 17:26:17 +0100

    Add insert and delete methods to BalancedBST class

--- src/BinarySearchTrees/BalancedBST.ts
+++ src/BinarySearchTrees/BalancedBST.ts
@@ -5,8 +5,12 @@ class TreeNode<T> {
   constructor(public value: T) {}
 }
 
-class BalancedBST<T> {
+export class BalancedBST<T extends number> {
   private root: TreeNode<T> | null = null;
+
+  public get testRoot(): TreeNode<T> | null {
+    return this.root;
+  }
 
   constructor(array: T[]) {
     this.buildTree(array);
@@ -32,6 +36,53 @@ class BalancedBST<T> {
     node.right = this.buildTreeHelper(array, mid + 1, end);
 
     return node;
+  }
+
+  public insert(value: T): void {
+    this.root = this.insertRecursive(this.root, value);
+  }
+
+  private insertRecursive(node: TreeNode<T> | null, value: T): TreeNode<T> {
+    if (node === null) return new TreeNode(value);
+
+    if (value < node.value) node.left = this.insertRecursive(node.left, value);
+    else if (value > node.value)
+      node.right = this.insertRecursive(node.right, value);
+
+    return node;
+  }
+
+  public delete(value: T): void {
+    this.root = this.deleteRecursive(this.root, value);
+  }
+
+  private deleteRecursive(
+    node: TreeNode<T> | null,
+    value: T
+  ): TreeNode<T> | null {
+    if (node === null) return null;
+
+    if (value < node.value) node.left = this.deleteRecursive(node.left, value);
+    else if (value > node.value)
+      node.right = this.deleteRecursive(node.right, value);
+    else {
+      if (node.left === null) return node.right;
+      else if (node.right === null) return node.left;
+
+      node.value = this.findMin(node.right).value;
+
+      node.right = this.deleteRecursive(node.right, node.value);
+    }
+
+    return node;
+  }
+
+  private findMin(node: TreeNode<T>): TreeNode<T> {
+    let current = node;
+
+    while (current.left !== null) current = current.left;
+
+    return current;
   }
 }
 
