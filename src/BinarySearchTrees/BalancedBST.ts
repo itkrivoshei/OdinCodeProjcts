b2d3821b7db7a390548ef20700d05b75bd200749
itkrivoshei <nikitakrivoshei@gmail.com>
2024-02-06 13:16:47 +0100

    Refactor prettyPrint function in BalancedBST.ts

--- src/BinarySearchTrees/BalancedBST.ts
+++ src/BinarySearchTrees/BalancedBST.ts
@@ -91,14 +91,13 @@ const prettyPrint = <T>(
   prefix = '',
   isLeft = true
 ) => {
-  if (node === null) {
-    return;
-  }
-  if (node.right !== null) {
+  if (node === null) return;
+
+  if (node.right !== null)
     prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-  }
+
   console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
-  if (node.left !== null) {
+
+  if (node.left !== null)
     prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-  }
 };
