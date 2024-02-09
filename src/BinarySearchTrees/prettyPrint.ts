769634a4d48bb56075b67bbf144be30d0f525310
itkrivoshei <nikitakrivoshei@gmail.com>
2024-02-09 10:17:13 +0100

    Add prettyPrint function to BinarySearchTrees

---
+++ src/BinarySearchTrees/prettyPrint.ts
@@ -0,0 +1,17 @@
+import { TreeNode } from './TreeNode';
+
+export const prettyPrint = <T>(
+  node: TreeNode<T> | null,
+  prefix = '',
+  isLeft = true
+) => {
+  if (node === null) return;
+
+  if (node.right !== null)
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
+
+  if (node.left !== null)
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+};
